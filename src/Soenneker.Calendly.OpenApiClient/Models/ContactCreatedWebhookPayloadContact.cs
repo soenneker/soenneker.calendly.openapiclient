997c416910bc8762314826b4909765174f9e894e
Jake Soenneker <jake@soenneker.com>
2026-07-29 18:01:21 +0000

    Automated update

--- src/Soenneker.Calendly.OpenApiClient/Models/ContactCreatedWebhookPayloadContact.cs
+++ src/Soenneker.Calendly.OpenApiClient/Models/ContactCreatedWebhookPayloadContact.cs
@@ -49,6 +49,14 @@ namespace Soenneker.Calendly.OpenApiClient.Models
 #else
         public string CreationSource { get; set; }
 #endif
+        /// <summary>Custom field values</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public List<global::Soenneker.Calendly.OpenApiClient.Models.ContactCreatedWebhookPayloadContactCustomFieldsItemProperty>? CustomFields { get; set; }
+#nullable restore
+#else
+        public List<global::Soenneker.Calendly.OpenApiClient.Models.ContactCreatedWebhookPayloadContactCustomFieldsItemProperty> CustomFields { get; set; }
+#endif
         /// <summary>The users&apos; email addresses. Max 10.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -155,6 +163,7 @@ namespace Soenneker.Calendly.OpenApiClient.Models
                 { "country", n => { Country = n.GetStringValue(); } },
                 { "created_at", n => { CreatedAt = n.GetDateTimeOffsetValue(); } },
                 { "creation_source", n => { CreationSource = n.GetStringValue(); } },
+                { "custom_fields", n => { CustomFields = n.GetCollectionOfObjectValues<global::Soenneker.Calendly.OpenApiClient.Models.ContactCreatedWebhookPayloadContactCustomFieldsItemProperty>(global::Soenneker.Calendly.OpenApiClient.Models.ContactCreatedWebhookPayloadContactCustomFieldsItemProperty.CreateFromDiscriminatorValue)?.AsList(); } },
                 { "emails", n => { Emails = n.GetCollectionOfObjectValues<global::Soenneker.Calendly.OpenApiClient.Models.Email2>(global::Soenneker.Calendly.OpenApiClient.Models.Email2.CreateFromDiscriminatorValue)?.AsList(); } },
                 { "first_email_date", n => { FirstEmailDate = n.GetDateTimeOffsetValue(); } },
                 { "job_title", n => { JobTitle = n.GetStringValue(); } },
@@ -183,6 +192,7 @@ namespace Soenneker.Calendly.OpenApiClient.Models
             writer.WriteStringValue("country", Country);
             writer.WriteDateTimeOffsetValue("created_at", CreatedAt);
             writer.WriteStringValue("creation_source", CreationSource);
+            writer.WriteCollectionOfObjectValues<global::Soenneker.Calendly.OpenApiClient.Models.ContactCreatedWebhookPayloadContactCustomFieldsItemProperty>("custom_fields", CustomFields);
             writer.WriteCollectionOfObjectValues<global::Soenneker.Calendly.OpenApiClient.Models.Email2>("emails", Emails);
             writer.WriteDateTimeOffsetValue("first_email_date", FirstEmailDate);
             writer.WriteStringValue("job_title", JobTitle);
